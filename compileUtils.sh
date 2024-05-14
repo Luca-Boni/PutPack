911e0d33f5070239c59a897f049fd70eeb690bd2
Luca-Boni <lucasartoriboni@hotmail.com>
2024-05-14 11:24:07 -0300

    Compile scripts create bin and obj folders if they don't exist

--- compileUtils.sh
+++ compileUtils.sh
@@ -1,5 +1,15 @@
 curPath=$(dirname $0)
 
+if [ ! -d $curPath'/obj' ]
+then
+    mkdir $curPath'/obj'
+fi
+
+if [ ! -d $curPath'/obj/Utils' ]
+then
+    mkdir $curPath'/obj/Utils'
+fi
+
 for file in $curPath/src/Utils/*.cpp
 do
     filename=${file##*/}
